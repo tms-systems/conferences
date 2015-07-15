d56f9b7337820a831f8c5a369f426ee54ad74e5f
reagantrose <reagantrose@gmail.com>
2015-07-15 12:29:51 -0700

    Update table.js

--- js/table.js
+++ js/table.js
@@ -20,6 +20,7 @@ function addMonthMenu() {
   $('.month-table').each(function(_, table) {
     var month = $(table).data('month')
     $('#cal-controls').append('<a class="month-menuitem" data-target="' + month + '" href="#' + month + '">' + month + '</a>')
+    $('#agenda').append('<div>' + eventStartDate + '</div>')
   })
 
   $(document).on('click', '.month-menuitem', function(e) {
