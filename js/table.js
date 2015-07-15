9aea430757d9311298d499918c8a206fa763f529
reagantrose <reagantrose@gmail.com>
2015-07-15 12:06:51 -0700

    Update table.js

--- js/table.js
+++ js/table.js
@@ -42,7 +42,7 @@ function addMonthMenu() {
 function appendEvent( event ) {
   var eventStartDate = new Date(event.startdate)
   var eventEndDate   = new Date(event.enddate)
-  var eventElement   = $('<div class="event"><a target="_blank" href="' + event.tickets + '"></a></div>')
+  var eventElement   = $('<div class="event"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
 
   // Handle multi-days
   if ( eventEndDate.getDate() ) {
@@ -66,7 +66,7 @@ function appendEvent( event ) {
         dateElement.append('<div class="event spacer">&nbsp;</div>')
       })
 
-      dateElement.removeClass('no-event').append('<div class="event multi-days following-days" title="' + event.name + '"><a target="_blank" href="' + event.tickets + '">wut</a></div>')
+      dateElement.removeClass('no-event').append('<div class="event multi-days following-days" title="' + event.name + '"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
     }
   }
 
