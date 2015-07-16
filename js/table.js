cdc572f1a02e2ab012e9bf4e1348f99e18d1f25e
reagantrose <reagantrose@gmail.com>
2015-07-16 09:22:29 -0700

    Update table.js

--- js/table.js
+++ js/table.js
@@ -15,29 +15,6 @@ function generateCalendar (eventData) {
   addMonthMenu()
 }
 
-// function addMonthMenu() {
-//   $('#calendar-goes-here').prepend('<div id="cal-controls">')
-//   $('.month-table').each(function(_, table) {
-//     var month = $(table).data('month')
-//     $('#cal-controls').append('<a class="month-menuitem" data-target="' + month + '" href="#' + month + '">' + month + '</a>')
-//   })
-
-//   $(document).on('click', '.month-menuitem', function(e) {
-//     $('[data-month]').hide()
-//     $('[data-month="' + $(this).data('target') + '"]').show()
-//     $(this).addClass('active').siblings().removeClass('active')
-//     e.preventDefault()
-//   })
-
-//   // Get current month and click it
-//   var currentMonth = $('[data-target=' + (new Date()).getFullYear() + "-"  + monthNames[(new Date()).getMonth()] + ']')
-//   if( currentMonth.length ) {
-//     currentMonth.click()
-//   } else {
-//     $('[data-target]').first().click()
-//   }
-// }
-
 function appendEvent( event ) {
   var eventStartDate = new Date(event.startdate)
   var eventEndDate   = new Date(event.enddate)
@@ -106,6 +83,8 @@ function generateMonthTable( date ) {
   monthTable.before('<h2 data-month="' + date.getFullYear() + '-' + eventMonthName + '">' + eventMonthName + ' ' + date.getFullYear() + '</h2>')
   
   $('#calendar-goes-here').append(agendaTable)
+  agendaTable.append('<tbody></tdbody>')
+  
   
   // Add month calendar header
   monthTableBody.append('<tr class="header"></tr>')
