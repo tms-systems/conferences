9470b0f85d3a59a10bb23b592611ad00c94db3ea
reagantrose <reagantrose@gmail.com>
2015-07-16 09:11:23 -0700

    Update table.js

--- js/table.js
+++ js/table.js
@@ -8,7 +8,6 @@ function generateCalendar (eventData) {
   generateAllTheMonths(eventData)
   eventData.forEach(function (event) {
     appendEvent(event)
-    appendEvent(event)
   })
 
   // Highlight today
@@ -16,28 +15,28 @@ function generateCalendar (eventData) {
   addMonthMenu()
 }
 
-function addMonthMenu() {
-  $('#calendar-goes-here').prepend('<div id="cal-controls">')
-  $('.month-table').each(function(_, table) {
-    var month = $(table).data('month')
-    $('#cal-controls').append('<a class="month-menuitem" data-target="' + month + '" href="#' + month + '">' + month + '</a>')
-  })
+// function addMonthMenu() {
+//   $('#calendar-goes-here').prepend('<div id="cal-controls">')
+//   $('.month-table').each(function(_, table) {
+//     var month = $(table).data('month')
+//     $('#cal-controls').append('<a class="month-menuitem" data-target="' + month + '" href="#' + month + '">' + month + '</a>')
+//   })
 
-  $(document).on('click', '.month-menuitem', function(e) {
-    $('[data-month]').hide()
-    $('[data-month="' + $(this).data('target') + '"]').show()
-    $(this).addClass('active').siblings().removeClass('active')
-    e.preventDefault()
-  })
+//   $(document).on('click', '.month-menuitem', function(e) {
+//     $('[data-month]').hide()
+//     $('[data-month="' + $(this).data('target') + '"]').show()
+//     $(this).addClass('active').siblings().removeClass('active')
+//     e.preventDefault()
+//   })
 
-  // Get current month and click it
-  var currentMonth = $('[data-target=' + (new Date()).getFullYear() + "-"  + monthNames[(new Date()).getMonth()] + ']')
-  if( currentMonth.length ) {
-    currentMonth.click()
-  } else {
-    $('[data-target]').first().click()
-  }
-}
+//   // Get current month and click it
+//   var currentMonth = $('[data-target=' + (new Date()).getFullYear() + "-"  + monthNames[(new Date()).getMonth()] + ']')
+//   if( currentMonth.length ) {
+//     currentMonth.click()
+//   } else {
+//     $('[data-target]').first().click()
+//   }
+// }
 
 function appendEvent( event ) {
   var eventStartDate = new Date(event.startdate)
