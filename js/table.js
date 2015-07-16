51b95127f223a0a2497b51f950b30dc562f6a5c9
reagantrose <reagantrose@gmail.com>
2015-07-16 09:54:40 -0700

    Update table.js

--- js/table.js
+++ js/table.js
@@ -83,7 +83,7 @@ function generateMonthTable( date ) {
   monthTable.before('<h2 data-month="' + date.getFullYear() + '-' + eventMonthName + '">' + eventMonthName + ' ' + date.getFullYear() + '</h2>')
   
   $('#calendar-goes-here').append(agendaTable)
-  agendaTable.append('<tbody><tr>').append('</tr></tbody>')
+  agendaTable.append('<tbody><tr><td>Oh no</td>').append('</tr></tbody>')
   
   
   // Add month calendar header
